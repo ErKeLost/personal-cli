523f501566043be5bcf84845d4c61e16e58d74b5
erkelost <1256029807@qq.com>
2024-04-04 12:29:40 +0800

    chore: add other command

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,25 @@ import {
   version,
   description,
 } from '../package.json' assert { type: 'json' }
+
+const main = defineCommand({
+  meta: {
+    name: "citty",
+    version: "1.0.0",
+    description: "Citty playground CLI",
+  },
+  setup() {
+    console.log("Setup");
+  },
+  cleanup() {
+    console.log("Cleanup");
+  },
+  subCommands: {
+    start: () => import("./commands/start").then((r) => r.default),
+    build: () => import("./commands/build").then((r) => r.default),
+    preview: () => import("./commands/preview").then((r) => r.default),
+    clean: () => import("./commands/clean").then((r) => r.default),
+  },
+});
+
+runMain(main);
