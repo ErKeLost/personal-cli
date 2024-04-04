16c0f17437a05950cd1c9892c784c10973ef5806
erkelost <1256029807@qq.com>
2024-04-04 12:31:42 +0800

    chore: add other command

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,42 @@ import {
 
 const main = defineCommand({
   meta: {
-    name: "citty",
-    version: "1.0.0",
-    description: "Citty playground CLI",
+    name: 'rolldown',
+    version,
+    description,
   },
+  args: {
+    config: {
+      type: 'string',
+      alias: 'c',
+      description:
+        'Use this config file (if argument is used but value is unspecified, defaults to rolldown.config.js)',
+    },
+    help: {
+      type: 'boolean',
+      alias: 'h',
+      description: 'Show this help message',
+    },
+  },
+  async run(ctx) {
+    if (ctx.args.help) {
+      await showUsage(ctx.cmd)
+      return
+    }
+
+    const { configPath } = parseArgs(ctx.args)
+
+    const config = await loadConfig(configPath)
+
+    if (!config) {
+      consola.error(`No configuration found at ${configPath}`)
+      process.exit(1)
+    }
+
+    await bundle(config)
+  },
+})
+
   setup() {
     console.log("Setup");
   },
