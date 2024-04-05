c5a80c98add349783e88512128b8268ac38a7a4a
erkelost <1256029807@qq.com>
2024-04-05 12:51:54 +0800

    chore: update create-template command

--- src/commands/create.ts
+++ src/commands/create.ts
@@ -1,1 +1,120 @@
-export default {}
+import { defineCommand } from "citty";
+import fs from 'node:fs'
+import prompts from 'prompts';
+import colors from 'picocolors';
+import { startArgs as args } from "../args.js";
+import { copyTemplate, formatTargetDir, getConfigPath, isEmpty, pkgFromUserAgent, resolveCommandOptions, shouldUsePnpm, shouldUseYarn } from "../utils.js";
+
+export default defineCommand({
+  meta: {
+    name: "create template",
+    description: "create a new project from a template with farm",
+  },
+  args,
+  run({ args }) {
+
+  },
+});
+
+interface IResultType {
+  packageName?: string;
+  projectName?: string;
+  framework?: string;
+  argFrameWork?: string;
+  autoInstall?: boolean;
+  packageManager?: string;
+}
+
+
+async function createFarm(args: any) {
+  const cwd = process.cwd();
+  const isYarnInstalled = shouldUseYarn();
+  const isPnpmInstalled = shouldUsePnpm();
+  const pkgInfo = pkgFromUserAgent(process.env.npm_config_user_agent);
+  const DEFAULT_TARGET_NAME = 'farm-project';
+  const argProjectName = formatTargetDir(args._[0]);
+  const argFramework = args.template || args.t;
+  let targetDir = argProjectName || DEFAULT_TARGET_NAME;
+  let result: IResultType = {};
+  const skipInstall = args['skip-install'] ?? args.skipInstall ?? true;
+  try {
+    result = await prompts(
+      [
+        {
+          type: argProjectName ? null : 'text',
+          name: 'projectName',
+          message: 'Project name:',
+          initial: DEFAULT_TARGET_NAME,
+          onState: (state) => {
+            targetDir = formatTargetDir(state.value) || DEFAULT_TARGET_NAME;
+          }
+        },
+        {
+          type: () =>
+            !fs.existsSync(targetDir) || isEmpty(targetDir) ? null : 'confirm',
+          name: 'overwrite',
+          message: () =>
+            (targetDir === '.'
+              ? '🚨 Current directory'
+              : `🚨 Target directory "${targetDir}"`) +
+            ` is not empty. Overwrite existing files and continue?`
+        },
+        {
+          type: (_, { overwrite }: { overwrite?: boolean }) => {
+            if (overwrite === false) {
+              throw new Error(colors.red('❌') + ' Operation cancelled');
+            }
+            return null;
+          },
+          name: 'overwriteChecker'
+        },
+        {
+          type: argFramework ? null : 'select',
+          name: 'framework',
+          message: 'Select a framework:',
+          initial: 0,
+          choices: [
+            {
+              title: colors.cyan('React'),
+              value: 'react'
+            },
+            { title: colors.green('Vue'), value: 'vue' },
+          ]
+        },
+        {
+          type: pkgInfo || skipInstall ? null : 'select',
+          name: 'packageManager',
+          message: 'Which package manager do you want to use?',
+          choices: [
+            { title: 'npm', value: 'npm' },
+            {
+              title: isYarnInstalled ? 'Yarn' : 'Yarn (not installed)',
+              value: 'yarn',
+              disabled: !isYarnInstalled
+            },
+            {
+              title: isPnpmInstalled ? 'Pnpm' : 'Pnpm (not installed)',
+              value: 'pnpm',
+              disabled: !isPnpmInstalled
+            }
+          ]
+        }
+      ],
+      {
+        onCancel: () => {
+          throw new Error(colors.red('❌') + ' Operation cancelled');
+        }
+      }
+    );
+  } catch (cancelled) {
+    console.log(cancelled.message);
+    return;
+  }
+  const { framework = argFramework, packageManager } = result;
+
+  await copyTemplate(targetDir, {
+    framework,
+    projectName: targetDir,
+    packageManager
+  });
+}
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,7 @@
 import path from "node:path";
+import { execSync } from 'child_process';
+import { fileURLToPath } from "node:url";
+import fs from 'node:fs'
 
 export function getConfigPath(configPath: string) {
   return path.join(process.cwd(), configPath ?? '');
@@ -35,4 +38,115 @@ export function cleanOptions(options: any) {
   delete resolveOptions.clearScreen;
 
   return resolveOptions;
+}
+
+export function pkgFromUserAgent(userAgent: string | undefined) {
+  if (!userAgent) return undefined;
+  const pkgSpec = userAgent.split(' ')[0];
+  const pkgSpecArr = pkgSpec.split('/');
+  return {
+    name: pkgSpecArr[0],
+    version: pkgSpecArr[1]
+  };
+}
+
+export function shouldUsePnpm(): boolean {
+  try {
+    const userAgent = process.env.npm_config_user_agent;
+    if (userAgent && userAgent.startsWith('pnpm')) {
+      return true;
+    }
+    execSync('pnpm --version', { stdio: 'ignore' });
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+export function shouldUseYarn(): boolean {
+  try {
+    const userAgent = process.env.npm_config_user_agent;
+    if (userAgent && userAgent.startsWith('yarn')) {
+      return true;
+    }
+    execSync('yarnpkg --version', { stdio: 'ignore' });
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+
+export async function copyTemplate(targetDir: string, options: any) {
+  const dest = path.join(process.cwd(), targetDir);
+  const templatePath = path.join(
+    fileURLToPath(import.meta.url),
+    `../templates/${options.framework}`
+  );
+  copy(templatePath, dest);
+
+  writePackageJson(dest, options);
+}
+
+function writePackageJson(dest: string, options: any) {
+  const pkg = JSON.parse(
+    fs.readFileSync(path.join(dest, `package.json`), 'utf-8')
+  );
+
+  pkg.name = options.projectName;
+
+  const currentPkgManager = getCurrentPkgManager(options);
+  if (currentPkgManager === 'yarn') {
+    pkg.scripts = pkg.scripts ?? {};
+    pkg.scripts.postinstall = 'npx --yes peer-gear --install';
+  }
+
+  const packageJsonPath = path.join(dest, 'package.json');
+  const { name, ...rest } = pkg;
+  const sortedPackageJson = { name, ...rest };
+  fs.writeFileSync(
+    packageJsonPath,
+    JSON.stringify(sortedPackageJson, null, 2) + '\n'
+  );
+}
+
+function getCurrentPkgManager(options: any) {
+  const pkgInfo = pkgFromUserAgent(process.env.npm_config_user_agent);
+  const pkgManager = pkgInfo ? pkgInfo.name : 'npm';
+  const currentPkgManager =
+    (pkgInfo ? pkgManager : options.packageManager) ?? 'npm';
+  return currentPkgManager;
+}
+
+
+function copy(src: string, dest: string) {
+  const stat = fs.statSync(src);
+  if (stat.isDirectory()) {
+    copyDir(src, dest);
+  } else {
+    fs.copyFileSync(src, dest);
+  }
+}
+
+function copyDir(srcDir: string, destDir: string) {
+  fs.mkdirSync(destDir, { recursive: true });
+  for (const file of fs.readdirSync(srcDir)) {
+    const srcFile = path.resolve(srcDir, file);
+    const destFile = path.resolve(destDir, file);
+    if (file === 'gitignore') {
+      copy(srcFile, destFile);
+      fs.renameSync(destFile, path.resolve(destDir, '.gitignore'));
+    } else {
+      copy(srcFile, destFile);
+    }
+  }
+}
+
+export function formatTargetDir(targetDir: string | undefined) {
+  return targetDir?.trim().replace(/\/+$/g, '');
+}
+
+export function isEmpty(path: string) {
+  const files = fs.readdirSync(path);
+  return files.length === 0 || (files.length === 1 && files[0] === '.git');
 }
